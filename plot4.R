56293399728aeda260e288b4d58cd8a4fdc7ec64
smiarnau <smiarnau@gmail.com>
2015-06-07 17:39:29 -0500

    final plot4

--- plot4.R
+++ plot4.R
@@ -8,14 +8,14 @@ newData$V3 <- as.numeric(as.character(newData$V3))
 dateTime <- as.POSIXlt(paste(as.Date(newData$V1, format="%d/%m/%Y"), newData$V2, sep=" "))
 
 par(mfrow = c(2, 2))
-
+par(bg = "white")
 plot(dateTime, newData$V3, type="l",xlab="", ylab="Global Active Power")
 plot(dateTime, newData$V5, type="l",xlab="datetime", ylab="Voltage")
     
 with(newData,plot(dateTime, newData$V7, type="l",xlab="", ylab="Energy sub metering"))
 with(subset(newData,select = c("V8")),lines(dateTime, newData$V8, type="l", col="red"))
 with(subset(newData,select = c("V9")),lines(dateTime, newData$V9, type="l", col="blue"))
-legend("topright", pch="-", lwd=1, adj=0.1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", pch="-", lwd=1, adj=0.1, box.lty=0, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 plot(dateTime, newData$V4, type="l",xlab="datetime", ylab="Global_reactive_power")
 
